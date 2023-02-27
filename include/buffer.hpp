f51e5e96cdde9ec5eeeb948eca55d8a8400b2b69
Enchan <enchant0130@gmail.com>
2023-02-27 11:11:59 +0900

    [Update] add ring-buffer

---
+++ include/buffer.hpp
@@ -0,0 +1,162 @@
+//
+// リングバッファ
+//
+
+#ifndef _COLLECTION2_BUFFER_H_
+#define _COLLECTION2_BUFFER_H_
+
+#include <climits>
+#include <cstdint>
+
+#include "common.hpp"
+
+namespace collection2 {
+
+/**
+ * @brief バッファサイズを管理する変数の型
+ */
+using buffer_size_t = uint16_t;
+
+/**
+ * @brief リングバッファ
+ *
+ * @tparam Element 扱う要素の型
+ */
+template <typename Element>
+class Buffer {
+   private:
+    /**
+     * @brief 内部データ管理領域
+     */
+    Element* const internalData;
+
+    /**
+     * @brief 内部データ長さ
+     */
+    buffer_size_t internalDataSize;
+
+    /**
+     * @brief バッファ先頭
+     */
+    buffer_size_t head = 0;
+
+    /**
+     * @brief バッファ末尾
+     */
+    buffer_size_t tail = 0;
+
+    /**
+     * @brief 現在バッファ内に存在するデータ数
+     */
+    buffer_size_t amount = 0;
+
+   public:
+    /**
+     * @brief 内部データを扱う領域とそのサイズを指定してバッファを初期化
+     *
+     * @param data 内部データ保管用領域
+     * @param dataSize 領域サイズ
+     * @note 領域サイズは2の冪乗であるべきです。それ以外の値を指定した場合、2の冪数のうち領域のサイズを下回らない最大のものが選択されます(15 -> 8, 34 -> 32).
+     */
+    Buffer(Element* const data, const buffer_size_t& dataSize);
+
+    ~Buffer() = default;
+
+    /**
+     * @brief バッファの末尾にデータを追加
+     *
+     * @param data 追加するデータ
+     * @return OperationResult 操作結果
+     */
+    OperationResult append(const Element& data);
+
+    /**
+     * @brief バッファの先頭からデータを取り出し
+     *
+     * @param data 取り出したデータの格納先
+     * @return OperationResult 操作結果
+     */
+    OperationResult pop(Element* const data);
+
+    /**
+     * @brief バッファに値を追加できるか
+     *
+     * @return bool
+     */
+    bool hasSpace() const {
+        return amount < internalDataSize;
+    }
+
+    /**
+     * @brief バッファが空かどうか
+     *
+     * @return bool
+     */
+    bool isEmpty() const {
+        return amount == 0;
+    }
+};
+
+template <typename Element>
+Buffer<Element>::Buffer(Element* const data, const buffer_size_t& dataSize) : internalData(data) {
+    // ゼロ長のバッファなら何もしない
+    if (dataSize == 0) {
+        internalDataSize = dataSize;
+        return;
+    }
+
+    // 与えられたサイズを上回らない最大の2の冪数を探す
+    const uint8_t BufferSizeBitLength = sizeof(buffer_size_t) * CHAR_BIT;  // バッファサイズのビット数
+    uint8_t currentBitPosition = BufferSizeBitLength;
+    buffer_size_t candidate = 0;
+    do {
+        candidate = static_cast<buffer_size_t>(1 << currentBitPosition);
+        if ((candidate & dataSize) != 0) {
+            break;
+        }
+    } while (currentBitPosition--);
+    internalDataSize = candidate;
+};
+
+template <typename Element>
+OperationResult Buffer<Element>::append(const Element& data) {
+    // サイズ0のバッファに値を追加することはできない
+    if (internalDataSize == 0) {
+        return OperationResult::Overflow;
+    }
+
+    // バッファがいっぱいなら、要素を一つ読み捨てる
+    if (!hasSpace()) {
+        auto result = pop(nullptr);
+    }
+
+    // tailの位置にデータを書き込む
+    *(internalData + tail) = data;
+
+    tail = (tail + 1) & (internalDataSize - 1);
+    amount++;
+
+    return OperationResult::Success;
+}
+
+template <typename Element>
+OperationResult Buffer<Element>::pop(Element* const data) {
+    // バッファが空なら戻る
+    if (isEmpty()) {
+        return OperationResult::Empty;
+    }
+
+    // 読み出して渡す。nullptrなら何もしないでheadを進める。
+    if (data != nullptr) {
+        *data = *(internalData + head);
+    }
+
+    head = (head + 1) & (internalDataSize - 1);
+    amount--;
+
+    return OperationResult::Success;
+}
+
+}  // namespace collection2
+
+#endif
---
+++ tests/test_buffer.cpp
@@ -0,0 +1,99 @@
+//
+// バッファのテスト
+//
+
+#include <gtest/gtest.h>
+
+#include "buffer.hpp"
+
+using namespace collection2;
+
+TEST(BufferTest, testInstantiate) {
+    // プリミティブ型
+    char charBufferData[8] = {0};
+    Buffer<char> charBuffer(charBufferData, 8);
+
+    // 構造体
+    struct Structure {
+        int integer;
+        char charactor;
+    };
+    Structure structBufferData[8] = {0};
+    Buffer<struct Structure> structBuffer(structBufferData, 8);
+}
+
+TEST(BufferTest, testOperateWithOrdinallySize) {
+    // int型を扱うバッファを定義
+    const int BufferLength = 8;
+    int bufferData[BufferLength] = {0};
+    Buffer<int> buffer(bufferData, BufferLength);
+
+    // バッファの長さだけ値を追加
+    for (int i = 0; i < BufferLength; i++) {
+        EXPECT_EQ(buffer.append(i + 1), OperationResult::Success);
+    }
+
+    // 範囲外の追加にも成功するが、最初の要素が読み捨てられる
+    EXPECT_EQ(buffer.append(9), OperationResult::Success);
+
+    // 値を取り出す
+    for (int i = 1; i < BufferLength + 1; i++) {
+        int value = -1;
+        EXPECT_EQ(buffer.pop(&value), OperationResult::Success);
+        EXPECT_EQ(value, i + 1);
+    }
+
+    // 範囲外の取り出し
+    int dummy = 0;
+    EXPECT_EQ(buffer.pop(&dummy), OperationResult::Empty);
+}
+
+TEST(BufferTest, testOperateWithExceptionalSize) {
+    // 2の冪数でない場合…
+    const int bufferLength = 100;
+    int bufferData[bufferLength] = {0};
+    Buffer<int> buffer(bufferData, bufferLength);
+
+    // バッファの長さに最も近い2の冪数回だけ値を追加
+    const int expectedBufferLength = 64;
+    for (int i = 0; i < expectedBufferLength; i++) {
+        EXPECT_EQ(buffer.append(i + 1), OperationResult::Success);
+    }
+
+    // 値を取り出す
+    for (int i = 0; i < expectedBufferLength; i++) {
+        int value = 0;
+        EXPECT_EQ(buffer.pop(&value), OperationResult::Success);
+        EXPECT_EQ(value, i + 1);
+    }
+
+    // 範囲外の取り出しとなるか?
+    int dummy = 0;
+    EXPECT_EQ(buffer.pop(&dummy), OperationResult::Empty);
+}
+
+TEST(BufferTest, testOperateWithZeroSize) {
+    // サイズ0のバッファ
+    Buffer<int> buffer(nullptr, 0);
+
+    // 値の追加・取り出しに失敗する
+    int value = 1;
+    EXPECT_EQ(buffer.append(value), OperationResult::Overflow);
+    EXPECT_EQ(buffer.pop(&value), OperationResult::Empty);
+}
+
+TEST(BufferTest, testOperateWithOneSize) {
+    // サイズ1のバッファ
+    const int bufferLength = 1;
+    int bufferData[bufferLength] = {0};
+    Buffer<int> buffer(bufferData, 1);
+
+    // 値の追加・取り出しが問題なく成功するか
+    EXPECT_EQ(buffer.append(1), OperationResult::Success);
+    EXPECT_EQ(buffer.append(2), OperationResult::Success);
+
+    int value = 0;
+    EXPECT_EQ(buffer.pop(&value), OperationResult::Success);
+    EXPECT_EQ(value, 2);
+    EXPECT_EQ(buffer.pop(&value), OperationResult::Empty);
+}
