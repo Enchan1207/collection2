65e96f8263f8d72a0aeb1e50f5b0b174bd2be764
Enchan <enchant0130@gmail.com>
2023-09-11 14:59:36 +0900

    [Fix #12] ISRs

--- tests_avr/isrs.cpp
+++ tests_avr/isrs.cpp
@@ -3,20 +3,56 @@
 //
 #include <avr/interrupt.h>
 
-#include <uart/uart.hpp>
+#include "uart/uart.hpp"
 
 using namespace uart;
 
-/**
- * 送信バッファ空き
- */
+#ifdef HAS_UART_0
+
+#ifdef USART0_UDRE_vect
+ISR(USART0_UDRE_vect) {
+#else
 ISR(USART_UDRE_vect) {
+#endif
     Serial0.onSendBufferEmpty();
 }
 
-/**
- * 受信完了
- */
+#ifdef USART0_RX_vect
+ISR(USART0_RX_vect) {
+#else
 ISR(USART_RX_vect) {
+#endif
     Serial0.onReceive();
 }
+
+#endif
+
+#ifdef HAS_UART_1
+ISR(USART1_UDRE_vect) {
+    Serial1.onSendBufferEmpty();
+}
+
+ISR(USART1_RX_vect) {
+    Serial1.onReceive();
+}
+#endif
+
+#ifdef HAS_UART_2
+ISR(USART2_UDRE_vect) {
+    Serial2.onSendBufferEmpty();
+}
+
+ISR(USART2_RX_vect) {
+    Serial2.onReceive();
+}
+#endif
+
+#ifdef HAS_UART_3
+ISR(USART3_UDRE_vect) {
+    Serial3.onSendBufferEmpty();
+}
+
+ISR(USART3_RX_vect) {
+    Serial3.onReceive();
+}
+#endif
